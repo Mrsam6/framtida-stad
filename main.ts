0991d3f4c625db544ffaae78be4e9e7e74685fdf
Mrsam6 <129512584+Mrsam6@users.noreply.github.com>
2023-03-31 13:41:07 +0200

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # # .
+        . . # # #
+        . # # # #
+        . # # # .
+        . # . # .
+        `)
+})
+led.plotBrightness(1240, 1420, 255)
